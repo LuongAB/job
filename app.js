c99ec97cccbe7b510f6b9f1882034611a3133220
Luong <LuongTran1421@gmail.com>
2023-04-29 23:18:25 +0700

    Update project

---
+++ app.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const xssClean = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const swaggerUI = require("swagger-ui-express");
+
+const yaml = require("yamljs");
+const swaggerDocument = yaml.load("./swagger.yaml");
+
+const app = express();
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const authMiddleware = require("./middleware/authentication");
+
+const authRoute = require("./routes/auth");
+const jobRoute = require("./routes/jobs");
+const connectDB = require("./db/connect");
+
+app.use(limiter);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xssClean());
+
+app.get("/", (req, res) => {
+	res.send(`<h1>Jobs API <a href="/api-docs">Documentation</a></h1>`);
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// extra packages
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/jobs", authMiddleware, jobRoute);
+
+// routes
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+	try {
+		await connectDB(process.env.DB_CONNECT);
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
